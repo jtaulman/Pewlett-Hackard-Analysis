246a44d80bab42ca6cb6ddd241e5c5b66de87cce
jtaulman <jtaulman23@gmail.com>
2022-02-13 10:25:39 -0500

    Updated Code

--- queries.sql
+++ queries.sql
@@ -143,9 +143,23 @@ FROM dept_manager AS dm
 SELECT  ce.emp_no,
 		ce.first_name,
 		ce.last_name,
--- INTO dept_info
+		d.dept_name
+INTO dept_info
 FROM current_emp as ce
 	INNER JOIN dept_emp AS de
 		ON (ce.emp_no = de.emp_no)
 	INNER JOIN departments AS d
 		ON (de.dept_no = d.dept_no);
+		
+-- List of Sales and Development Departments
+SELECT  ce.emp_no,
+		ce.first_name,
+		ce.last_name,
+		d.dept_name
+INTO sales_info
+FROM current_emp as ce
+	INNER JOIN dept_emp AS de
+		ON (ce.emp_no = de.emp_no)
+	INNER JOIN departments AS d
+		ON (de.dept_no = d.dept_no)
+WHERE d.dept_no IN ('d005', 'd007');
