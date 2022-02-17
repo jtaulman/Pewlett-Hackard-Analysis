0b0d0dd92f4c699565b3bd301fe40ce675beae45
jtaulman <jtaulman23@gmail.com>
2022-02-17 11:49:16 -0500

    Final Challenge Code

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -86,18 +86,20 @@ FROM dept_emp
 SELECT title
 FROM titles
 
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_np,
-first_name,
-last_name,
-birth_date,
-from_date,
-to_date
-title
---INTO mentorship_eligibility  
-FROM employees AS e
-INNER JOIN dep_no AS dn
-INNER JOIN titles AS t
-WHERE (to_date = CAST('9999-01-01' AS date)) 
-	AND (birth_date BETWEEN CAST('1965-01-01' AS date) AND CAST('1965-12-31' AS date))
-ORDER BY emp_no;
+-- Creating a list of employees eligible for potential mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+ e.first_name,
+ e.last_name,
+ e.birth_date,
+ de.from_date,
+ de.to_date,
+ ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
